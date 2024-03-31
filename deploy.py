c2e72e2028a60a5d943387122c27e8a3acdd0c28
Ola Billy <olabilly@olas-air.home>
2024-04-01 01:27:35 +0200

    cwb

--- deploy.py
+++ deploy.py
@@ -3,25 +3,25 @@
 import subprocess
 
 def main():
-    # navigate to your django project directory
+    # Navigate to your Django project directory
     project_dir = '/coded'
-    subprocess.run(['cd', project_dir], check=True)
+    subprocess.run(['cd', project_dir], check=True, shell=True)
 
-    #Activate virtualenv
+    # Activate virtualenv
     venv_activate = '/.venv/Scripts/activate'
     subprocess.run(['source', venv_activate], check=True, shell=True)
 
-    # pull the latest change from the git repository
-    subprocess.run(['git', 'pull', 'origin','main'], check=True)
+    # Pull the latest changes from the Git repository
+    subprocess.run(['git', 'pull', 'origin', 'main'], check=True)
 
-    # collect static files ( if necessary)
-    subprocess.run(['python','manage.py', 'collectstatic', '--noinput'], check=True)
+    # Collect static files (if necessary)
+    subprocess.run(['python', 'manage.py', 'collectstatic', '--noinput'], check=True)
 
-    #migrate the database
-    subprocess.run(['python','manage.py','migrate'], check=True)
+    # Migrate the database
+    subprocess.run(['python', 'manage.py', 'migrate'], check=True)
 
-    # Restart the development server
-    subprocess.run(['systemct1', 'restart', 'gunicorn'], check=True)
+    # Restart the development server (assuming you're using Gunicorn)
+    subprocess.run(['systemctl', 'restart', 'gunicorn'], check=True)
 
 if __name__ == '__main__':
     main()
