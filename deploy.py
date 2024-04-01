03e98e2abbb541ad0701e760a39b484632ae9d8d
Ola Billy <olabilly@olas-air.home>
2024-04-01 02:53:19 +0200

    cwb

--- deploy.py
+++ deploy.py
@@ -2,26 +2,41 @@
 
 import subprocess
 
-def main():
-    # Navigate to your Django project directory
-    project_dir = '/coded'
-    subprocess.run(['cd', project_dir], check=True, shell=True)
+# Array of commands
+commands = [
+    'echo $PWD',
+    'whoami',
+    'git pull',
+    'git status',
+    'git submodule sync',
+    'git submodule update',
+    'git submodule status'
+]
 
-    # Activate virtualenv
-    venv_activate = '/.venv/Scripts/activate'
-    subprocess.run(['source', venv_activate], check=True, shell=True)
+# Execute commands
+output = ''
+for command in commands:
+    tmp = subprocess.run(command, shell=True, capture_output=True, text=True)
+    output += f"<span style=\"color: #6BE234;\">$</span><span style=\"color: #729FCF;\">{command}\n</span><br />"
+    output += f"{tmp.stdout}\n<br /><br />"
 
-    # Pull the latest changes from the Git repository
-    subprocess.run(['git', 'pull', 'origin', 'main'], check=True)
+# HTML output
+html_output = f'''
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<div style="width:700px">
+    <div style="float:left;width:350px;">
+    <p style="color:white;">Git Deployment Script</p>
+    {output}
+    </div>
+</div>
+</body>
+</html>
+'''
 
-    # Collect static files (if necessary)
-    subprocess.run(['python', 'manage.py', 'collectstatic', '--noinput'], check=True)
-
-    # Migrate the database
-    subprocess.run(['python', 'manage.py', 'migrate'], check=True)
-
-    # Restart the development server (assuming you're using Gunicorn)
-    subprocess.run(['systemctl', 'restart', 'gunicorn'], check=True)
-
-if __name__ == '__main__':
-    main()
+print(html_output)
