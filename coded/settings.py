3a94fd8d47f83f00148900b8ba6406b053c1291e
Ola Billy <olabilly@olas-air.home>
2024-04-01 02:47:52 +0200

    cwb

--- coded/settings.py
+++ coded/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fe25+(e^6=c0^csxec8^0f=5fm6b2-evks-01!4sgc01u=d(hh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['64.23.225.152']
+ALLOWED_HOSTS = []
 
 
 # Application definition
