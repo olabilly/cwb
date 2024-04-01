2d62bd01088bc5e757f93b3439122615c4d6a348
Ola Billy <olabilly@olas-air.home>
2024-04-01 03:28:44 +0200

    cwb

--- coded/settings.py
+++ coded/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fe25+(e^6=c0^csxec8^0f=5fm6b2-evks-01!4sgc01u=d(hh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['64.23.225.152']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = '/var/www/html/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = 'static/'
 
