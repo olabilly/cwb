c1b2e4be3428b21a9e5af2f63fe7e1e65adee71f
Ola Billy <olabilly@olas-air.home>
2024-04-02 03:47:52 +0200

    Cwb

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Stop the current Django server
+sudo systemctl stop gunicorn
+
+# Activate your virtual environment (if any)
+source .venv/Scripts/activate
+
+# Navigate to your project directory
+# cd /path/to/your/project
+
+# Pull the latest changes from your Git repository
+git pull origin main
+
+# Install or update Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Migrate database changes
+python3 manage.py migrate
+
+# Restart the Django server
+sudo systemctl start gunicorn
