ed2e6721c8e68a184305fa45c9573d9504cf72d6
Ola Billy <olabilly@olas-air.home>
2024-04-02 04:03:43 +0200

    Cwb Done

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,10 @@ source .venv/Scripts/activate
 git pull
 
 # Install or update Python dependencies
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Migrate database changes
 python3 manage.py migrate
